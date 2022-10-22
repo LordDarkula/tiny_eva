5e2391c0eb8ae5e57f5d6f9cf8b5919c51bbf08a
Aubhro Sengupta <hello@aubhro.com>
2022-10-22 05:29:08 -0400

    Implement video decoding
    Use opencv to decode an mp4 into a set of jpeg frames.

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -4,16 +4,26 @@ import os
 import tiny_eva
 from tiny_eva.decoder import Video
 
-
-def test_recorder():
+def test_decoded_num_frames(tmp_path):
     package_path = Path(tiny_eva.__file__).parent
-    sample_video_location = package_path / "data" / "sample_640x360.mp4"
-    decoded_frames_location = package_path / "out"
+    sample_video_location = package_path.parent / "data" / "sample_640x360.mp4"
+    print(f"{package_path=}")
 
     video = Video(sample_video_location)
-    video.decode(decoded_frames_location)
+    video.decode(tmp_path)
 
-    for fname in os.listdir(decoded_frames_location):
-        if os.path.isfile(os.path.join(decoded_frames_location, fname)):
-            ext = os.path.splitext[1]
+    assert len(video) == 400
+
+def test_decoded_all_frames_jpg(tmp_path):
+    package_path = Path(tiny_eva.__file__).parent
+    sample_video_location = package_path / "data" / "sample_640x360.mp4"
+    print(f"{package_path=}")
+
+    video = Video(sample_video_location)
+    video.decode(tmp_path)
+
+    for fname in os.listdir(tmp_path):
+        frame_path = os.path.join(tmp_path, fname)
+        if os.path.isfile(frame_path):
+            ext = os.path.splitext(frame_path)[1]
             assert ext == ".jpg" or ext == ".jpeg"
--- tiny_eva/decoder.py
+++ tiny_eva/decoder.py
@@ -1,10 +1,28 @@
 from os import PathLike
 from pathlib import Path
-
+import cv2
 
 class Video:
     def __init__(self, source: PathLike) -> None:
         self.source = Path(source)
+        self.encoded_frames_location: Path = None
+        self._num_frames = - 1
 
-    def decode(self, destination: PathLike) -> None:
-        pass
+    def decode(self, destination_dir: PathLike) -> None:
+        self.encoded_frames_location = Path(destination_dir)
+        self.encoded_frames_location.mkdir(exist_ok=True)
+
+        vidcap = cv2.VideoCapture(str(self.source.resolve()))
+        success,image = vidcap.read()
+        count = 0
+        while success:
+            cv2.imwrite(f"{self.encoded_frames_location.resolve()}/frame{count}.jpg", image)
+            success,image = vidcap.read()
+            print('Read a new frame: ', success)
+            count += 1
+        self._num_frames = count
+
+    def __len__(self) -> int:
+        return self._num_frames
+
+
