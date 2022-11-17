743ddf83fdb7d80169cdc670604808003df0d2e3
Aubhro Sengupta <hello@aubhro.com>
2022-11-17 02:54:02 -0500

    Separate TorchHubLoader into separate class
    Convert UDF into factory to generate instances of subclasses of
    AbstractLoader.

--- tiny_eva/udf_loader.py
+++ tiny_eva/udf_loader.py
@@ -1,16 +1,10 @@
 from typing import Any, Callable
-from os import PathLike
 from enum import Enum, auto
 from abc import ABCMeta, abstractmethod
 
 import torch  # type: ignore
 
 from tiny_eva.frame import Frame
-
-
-class ModelSource(Enum):
-    CALLABLE = auto()
-    TORCH_HUB = auto()
 
 
 class AbstractLoader(metaclass=ABCMeta):
@@ -24,37 +18,30 @@ class CallableLoader(AbstractLoader):
         self._func = func
 
     def __call__(self, frame: Frame, **kwds: Any) -> Any:
-        return self._func(frame, *kwds)
+        return self._func(frame, **kwds)
 
 
 class TorchHubLoader(AbstractLoader):
-    pass
+    def __init__(self, model_uri: str, name: str, **kwds: Any) -> None:
+        self._torch_model = torch.hub.load(model_uri, name, **kwds)
+
+    def __call__(self, frame: Frame, **kwds: Any) -> Any:
+        return self._torch_model(frame.source).pandas().xyxy
 
 
 class UDF:
-    def __init__(self, func: Any, model_source: ModelSource) -> None:
-        self.func: Any = func
-        self.model_source: ModelSource = model_source
-
     @staticmethod
     def from_callable(func: Callable):
         return CallableLoader(func)
 
-    @classmethod
-    def from_torch_hub(cls, path: PathLike, name: str, pretrained: bool = True):
+    @staticmethod
+    def from_torch_hub(model_uri: str, name: str, pretrained: bool = True):
         """
         Loads model from pytorch hub. Downloads model, so this will take time.
 
         Arguments:
-            path: hub path to model repo
+            model_uri: hub path to model repo
             name: name of hub model
             pretrained: should fetched model be pretrained
         """
-        model = torch.hub.load(path, name, pretrained=pretrained)
-        return cls(model, ModelSource.TORCH_HUB)
-
-    def __call__(self, frame: Frame, **kwds: Any) -> Any:
-        if self.model_source == ModelSource.TORCH_HUB:
-            return self.func(frame.source).pandas().xyxy
-        else:
-            return self.func(frame, **kwds)
+        return TorchHubLoader(model_uri, name, pretrained=pretrained)
