71daf914784597a569f8b988ba67ad537110c290
Aubhro Sengupta <hello@aubhro.com>
2022-11-18 18:42:35 -0500

    Add stubs to make all_zeros integration tests compile

--- tests/test_all_zeros.py
+++ tests/test_all_zeros.py
@@ -1,9 +1,20 @@
 import pytest
 
+from tiny_eva.query import Query
+from tiny_eva.executor import NaiveExecutor
+
 
 @pytest.mark.skip
-def test_simple_query(blank_frame_video, all_zeros_udf):
-    query = Query().filter(all_zeros_udf).all()
+def test_all_query(blank_frame_video, all_zeros_udf):
+    query = Query().map(all_zeros_udf).filter(udf="all_zeros_udf", result=True).all()
     executor = NaiveExecutor()
     result_video = executor.execute(blank_frame_video, query)
-    assert len(result_video) == 40
+    assert len(result_video) == 15
+
+
+@pytest.mark.skip
+def test_limit_query(blank_frame_video, all_zeros_udf):
+    query = Query().map(all_zeros_udf).filter(udf="all_zeros_udf", result=True).limit(2)
+    executor = NaiveExecutor()
+    result_video = executor.execute(blank_frame_video, query)
+    assert len(result_video) == 2
---
+++ tiny_eva/executor.py
@@ -0,0 +1,2 @@
+class NaiveExecutor:
+    pass
