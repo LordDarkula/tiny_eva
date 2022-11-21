94681a4a5016275d83cdf2470902078de6937630
Aubhro Sengupta <hello@aubhro.com>
2022-11-21 01:37:13 -0500

    Convert from_source and from_numpy to staticmethods

--- tiny_eva/frame.py
+++ tiny_eva/frame.py
@@ -1,13 +1,10 @@
 from os import PathLike
-from pathlib import Path
-from typing import Any, Optional, Type, TypeVar
+from typing import TypeVar
 from abc import ABCMeta, abstractmethod
 
 import numpy as np  # type: ignore
 from numpy.typing import ArrayLike  # type: ignore
 from PIL import Image  # type: ignore
-
-FrameType = TypeVar("FrameType", bound="Frame")
 
 
 class AbstractFrame(metaclass=ABCMeta):
@@ -64,22 +61,6 @@ class Frame:
     A Frame must be either on-disk or in-memory but not both or neither.
     """
 
-    def __init__(
-        self, source: Optional[PathLike] = None, frame_array: Optional[ArrayLike] = None
-    ):
-        if source is None and frame_array is None:
-            raise ValueError(
-                "A Frame must be initialized with either a source path or a numpy array."
-            )
-
-        if source is not None and frame_array is not None:
-            raise ValueError(
-                "A Frame cannot be initializaed with both a source path and a numpy array."
-            )
-
-        self._source = Path(source) if source is not None else None
-        self._frame_array = frame_array
-
     @staticmethod
     def from_source(source: PathLike) -> JPEGFrame:
         """
@@ -87,36 +68,12 @@ class Frame:
         """
         return JPEGFrame(source)
 
-    @classmethod
-    def from_numpy(cls: Type[FrameType], frame_array: ArrayLike) -> FrameType:
+    @staticmethod
+    def from_numpy(frame_array: ArrayLike) -> NumpyFrame:
         """
         Create Frame object from a numpy array.
 
         Arguments:
             frame_array: np.array of shape (num_channels, height, width)
         """
-        return cls(frame_array=frame_array)
-
-    @property
-    def source(self) -> Optional[PathLike]:
-        return self._source
-
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Frame):
-            return False
-
-        return np.array_equal(self.to_numpy(), other.to_numpy())
-
-    def to_numpy(self) -> ArrayLike:
-        """
-        Convert Frame into numpy array.
-
-        Returns:
-            np.array of shape (num_channels, height, width)
-        """
-        if self._frame_array is None:
-            image = Image.open(self._source)
-            frame = np.asarray(image)
-            self._frame_array = np.transpose(frame, (2, 0, 1))
-
-        return self._frame_array
+        return NumpyFrame(frame_array)
