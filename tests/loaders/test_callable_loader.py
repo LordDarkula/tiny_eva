619b7ad98aa41b826b3def684f4df633f641c86e
Aubhro Sengupta <hello@aubhro.com>
2022-11-29 22:37:54 -0500

    Move CallableLoader tests to separate file

---
+++ tests/loaders/test_callable_loader.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np  # type: ignore
+
+from tiny_eva.frame import Frame
+
+
+@pytest.fixture
+def zeros_frame():
+    return Frame.from_numpy(np.zeros((3, 2, 3)))
+
+
+def test_all_zeros_len_is_one(zeros_frame, all_zeros_udf):
+    assert len(all_zeros_udf(zeros_frame)) == 1
+
+
+def test_all_zeros_true_on_zeros_frame(zeros_frame, all_zeros_udf):
+    assert all_zeros_udf(zeros_frame)
+
+
+def test_all_zeros_false_in_on_ones_frame(all_zeros_udf):
+    sample_frame = Frame.from_numpy(np.ones((3, 2, 3)))
+    assert not all_zeros_udf(sample_frame)
--- tests/loaders/test_udf_loader.py
+++ tests/loaders/test_udf_loader.py
@@ -1,13 +1,7 @@
-import numpy as np  # type: ignore
 import pytest
 
 from tiny_eva.loaders.udf_loader import UDF
 from tiny_eva.frame import Frame
-
-
-@pytest.fixture
-def zeros_frame():
-    return Frame.from_numpy(np.zeros((3, 2, 3)))
 
 
 @pytest.fixture(scope="session")
@@ -18,19 +12,6 @@ def yolo():
 @pytest.fixture(scope="session")
 def alexnet():
     return UDF.from_torch_hub("pytorch/vision", "alexnet", pretrained=True)
-
-
-def test_all_zeros_len_is_one(zeros_frame, all_zeros_udf):
-    assert len(all_zeros_udf(zeros_frame)) == 1
-
-
-def test_all_zeros_true_on_zeros_frame(zeros_frame, all_zeros_udf):
-    assert all_zeros_udf(zeros_frame)
-
-
-def test_all_zeros_false_in_on_ones_frame(all_zeros_udf):
-    sample_frame = Frame.from_numpy(np.ones((3, 2, 3)))
-    assert not all_zeros_udf(sample_frame)
 
 
 def test_yolo_result_len(yolo, traffic_frame_path):
