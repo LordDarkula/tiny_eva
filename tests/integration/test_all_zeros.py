7c49683f6fd92e249f4be57d38cad5f167a51fd5
Aubhro Sengupta <hello@aubhro.com>
2023-02-18 23:39:30 -0800

    Move all_zeros and num_black_pixels into integration tests directory

--- tests/integration/test_all_zeros.py
+++ tests/integration/test_all_zeros.py
@@ -4,12 +4,10 @@ from tiny_eva.query import Query
 from tiny_eva.executor import NaiveExecutor
 
 
-@pytest.mark.skip
 def test_all_query(blank_frame_video, all_zeros_udf):
-    query = Query().map(all_zeros_udf).filter(udf="all_zeros_udf", result=True).all()
-    executor = NaiveExecutor()
-    result_video = executor.execute(blank_frame_video, query)
-    assert len(result_video) == 15
+    query = Query().map(all_zeros_udf).filter(condition=all_zeros_udf)
+    result_video = query(blank_frame_video)
+    assert len(list(result_video)) == 15
 
 
 @pytest.mark.skip
